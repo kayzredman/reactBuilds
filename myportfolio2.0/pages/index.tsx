b3bcc914b8b0a64a078d42554a99af062164445f
Kwekku <Kayredman@gmail.com>
2023-01-13 10:53:23 +0000

    Boiler stage

--- myportfolio2.0/pages/index.tsx
+++ myportfolio2.0/pages/index.tsx
@@ -7,12 +7,11 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <>
+    <div>
       <Head>
         <title>Kayz Portfolio</title>
       </Head>
-
-      <h1>Welcome to The Build!!!</h1>
-    </>
+      <h1 className="text-blue-800">Hello World</h1>
+    </div>
   );
 }
