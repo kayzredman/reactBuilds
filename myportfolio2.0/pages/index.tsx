5d8670dc97d2680c093c3d38b3b2b7183764e37e
Kwekku <Kayredman@gmail.com>
2023-01-15 02:25:01 +0000

    After the hero section

--- myportfolio2.0/pages/index.tsx
+++ myportfolio2.0/pages/index.tsx
@@ -1,21 +1,25 @@
 import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "@next/font/google";
-import styles from "../styles/Home.module.css";
 import Header from "../components/Header";
+import Hero from "../components/Hero";
+import { motion } from "framer-motion";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Head>
         <title>Kayz Portfolio</title>
       </Head>
       {/* Header */}
       <Header />
       {/* Hero */}
+      <section id="hero" className="snap-center">
+        <Hero />
+      </section>
       {/* About */}
+
       {/* Experience */}
       {/* Skills */}
       {/* Projects */}
